4dda8b8d61e270a6881b7646833c83a8bf961350
Drkphl <leonardotizon@hotmail.com>
2024-07-01 20:38:00 -0300

    teste 1

--- aula22.js
+++ aula22.js
@@ -19,12 +19,18 @@
 // }
 // console.log(Numeros.join(""));
 
-// const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require("prompt-sync")({ sigint: true });
 
-// function maiorNumeroPossivel(num) {
-//     return parseInt(num.split("").sort((a, b) => b - a).join(""),10);
-// }
+function maiorNumeroPossivel(num) {
+    return parseInt(
+        num
+            .split("")
+            .sort((a, b) => b - a)
+            .join(""),
+        10
+    );
+}
 
-// const numero = prompt("Por favor, forneça um número: ");
-// const resultado = maiorNumeroPossivel(numero);
-// console.log("O maior número possível com os algarismos fornecidos é: " + resultado);
+const numero = prompt("Por favor, forneça um número: ");
+const resultado = maiorNumeroPossivel(numero);
+console.log("O maior número possível com os algarismos fornecidos é: " + resultado);
