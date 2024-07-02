a7883f6c9023f55cc69b8d4ca609306b2fac2d20
Drkphl <leonardotizon@hotmail.com>
2024-07-02 19:15:11 -0300

    final

--- prova6.js
+++ prova6.js
@@ -1,44 +1,66 @@
 let notas = [7.5, 8.0, 6.0, 9.5, 5.0, 8.5, 7.0, 10.0, 6.5, 9.0];
 let soma = 0;
-// let maiorNota = notas[0];
-// let menorNota = notas[0];
-// let notasAcimaMedia = 0;
-// let notasAbaixoMedia = 0;
+let maiorNota = notas[0];
+let menorNota = notas[0];
+let notasAcimaMedia = 0;
+let notasAbaixoMedia = 0;
 
 function total(num) {
     for (let i = 0; i < notas.length; i++) {
         soma += notas[i];
         return soma;
     }
-
-    //     function Maior() {
-    //         if (notas[i] > maiorNota) {
-    //             maiorNota = notas[i];
-    //         }
-    //     }
-
-    //     if (notas[i] < menorNota) {
-    //         menorNota = notas[i];
-    //     }
-
-    //     if (notas[i] >= 6) {
-    //         console.log(notas[i]);
-    //         notasAcimaMedia++;
-    //     } else {
-    //         notasAbaixoMedia++;
-    //     }
-    // }
-
-    // console.log(soma / notas.length); // ref A
-    // console.log(maiorNota, menorNota); // ref B
-    // console.log(notasAcimaMedia); // ref C
-    // console.log("reprovados:", notasAbaixoMedia); // ref E
-
-    // notas.push(8.0); // ref D
-
-    // // ref D
-    // for (let i = 0; i < notas.length; i++) {
-    //     console.log(notas[i]);
 }
 
+function Maior() {
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] > maiorNota) {
+            maiorNota = notas[i];
+        }
+    }
+    return maiorNota;
+}
+
+function Menor() {
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] < menorNota) {
+            menorNota = notas[i];
+        }
+    }
+    return menorNota;
+}
+
+function Acima() {
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] >= 6) {
+            notasAcimaMedia++;
+        }
+    }
+    return notasAcimaMedia;
+}
+function Abaixo() {
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] < 6) {
+            notasAbaixoMedia++;
+        }
+    }
+    return notasAbaixoMedia;
+}
+function adicionar() {
+    notas.push(8.0);
+}
+
+function imprimir() {
+    for (let i = 0; i < notas.length; i++) {
+        console.log(notas[i]);
+    }
+}
+
+adicionar();
 console.log(total());
+console.log(Maior());
+console.log(Menor());
+console.log(`temos ${Acima()} notas acima da média`);
+console.log(`temos ${Abaixo()} notas abaixo da média`);
+console.log(`aqui está a lista de notas:`);
+imprimir();
