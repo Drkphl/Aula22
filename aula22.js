a56c5c98eecadc81c218b108b796859dd2b05d1a
Drkphl <leonardotizon@hotmail.com>
2024-07-01 21:00:55 -0300

    testes final

--- aula22.js
+++ aula22.js
@@ -13,24 +13,54 @@
 //         if (Numeros[i] < Numeros[j]) {
 //             let ordem = Numeros[i];
 //             Numeros[i] = Numeros[j];
-//             Numeros[j] = ordem
+//             Numeros[j] = ordem;
 //         }
 //     }
+//     console.log(Numeros.join(""));
 // }
-// console.log(Numeros.join(""));
 
-const prompt = require("prompt-sync")({ sigint: true });
+// const prompt = require("prompt-sync")({ sigint: true });
 
-function maiorNumeroPossivel(num) {
-    return parseInt(
-        num
-            .split("")
-            .sort((a, b) => b - a)
-            .join(""),
-        10
-    );
+// function maiorNumeroPossivel(num) {
+//     return parseInt(
+//         num
+//             .split("")
+//             .sort((a, b) => b - a)
+//             .join(""),
+//         10
+//     );
+// }
+
+// const numero = prompt("Por favor, forneça um número: ");
+// const resultado = maiorNumeroPossivel(numero);
+// console.log("O maior número possível com os algarismos fornecidos é: " + resultado);
+
+const prompt = require("prompt-sync")();
+
+function ehNumero(num) {
+    return !isNaN(Number(num));
 }
 
-const numero = prompt("Por favor, forneça um número: ");
-const resultado = maiorNumeroPossivel(numero);
-console.log("O maior número possível com os algarismos fornecidos é: " + resultado);
+function maiorNumero(num) {
+    if (ehNumero(num)) {
+        let array = num.split("");
+
+        for (let i = 0; i < array.length; i++) {
+            for (let j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    let aux = array[j];
+                    array[j] = array[i];
+                    array[i] = aux;
+                }
+            }
+        }
+
+        return array.join("");
+    }
+    console.log("Número inválido");
+}
+
+const numero = prompt("Digite um número: ");
+
+let maior = maiorNumero(numero);
+console.log(maior);
